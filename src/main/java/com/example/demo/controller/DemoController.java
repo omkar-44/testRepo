0981b2443aabd14a547a75222c24c7b727641687
omkar <omkarby@gmail.com>
2022-09-04 16:45:44 +0530

    modified controller

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -2,7 +2,7 @@ package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+//omkar/c02/
 @RestController
 public class DemoController {
 //added comment from development
